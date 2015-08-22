9ba6d1539a0e3f2fd6fd48cb6684a83898ad3ce0
秉亮 <bl_indie@qq.com>
2015-08-23 00:49:25 +0800

    增加摇杆和其他按钮事件框架

--- arduino/controller/controller.ino
+++ arduino/controller/controller.ino
@@ -13,17 +13,73 @@ int stickBtn = 6;
 int stopBtn = 3;
 bool isStopBtnPressed = false;
 
+//基准量
+int benchmarkX = 512;
+int benchmarkY = 512;
+
+void benchmarkSetup(){
+  int i;
+  int xSum = 0;
+  int ySum = 0;
+  for(i = 0; i < 3; i++){
+    xSum += analogRead(axisX);
+    ySum += analogRead(axisY);
+    delay(100);
+  }
+  benchmarkX = xSum / i;
+  benchmarkY = ySum / i;
+}
+
 void setup(){
   Serial.begin(9600);
   //Serial.println("------------setup()-------------");
+  benchmarkSetup();
 }
 
+//公用临时变量
+int sensorValue;
+
 void loop(){
-  int sensorValue  = digitalRead(stopBtn);
+  sensorValue = digitalRead(stopBtn);
   if (sensorValue == 0||isStopBtnPressed == true){
     //按下停止按钮后改变判断值，进入死循环，只有重启可以解除该状态
     isStopBtnPressed = true;
     return;
   }
+  
+  sensorValue = digitalRead(rightBtn);
+  if (sensorValue == 0){
+    //按下了摄像头居中按钮
+    //...
+  }
+  else{
+    int upValue = digitalRead(upBtn);
+    int downValue = digitalRead(downBtn);
+    if(upValue != downValue){
+      //都只有一个按下一个不按下时才进入函数体
+      if(upValue == 0){
+        //按下了上按键
+        //...
+      }
+      else{
+        //按下了下按键
+        //...
+      }
+    }
+  }
+  
+  sensorValue = digitalRead(stickBtn);
+  if (sensorValue == 0){
+    //按下了摇杆按钮
+    //...
+  }
+
+  int xValue = analogRead(axisX);
+  int yValue = analogRead(axisY);
+  driveTracks(xValue, yValue);  
+}
+
+void driveTracks(int xValue, int yValue){
+  
 }
 
