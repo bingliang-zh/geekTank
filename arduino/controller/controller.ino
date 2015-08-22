6d7baed5bf347705b8eeceaaf67c457182bbbaec
秉亮 <bl_indie@qq.com>
2015-08-22 23:00:09 +0800

    增加停止键

--- arduino/controller/controller.ino
+++ arduino/controller/controller.ino
@@ -1,28 +1,29 @@
-int F = 2;
-int B = 4;
-int L = 3;
-int R = 5;
+//右边的按键
+int upBtn = 2;
+int downBtn = 4;
+int leftBtn = 3;
+int rightBtn = 5;
+
+//摇杆
+int axisX = A0;
+int axisY = A1;
+int stickBtn = 6;
+
+//特定功能键
+int stopBtn = 3;
+bool isStopBtnPressed = false;
 
 void setup(){
   Serial.begin(9600);
-  Serial.println("------------setup()-------------");
+  //Serial.println("------------setup()-------------");
 }
 
 void loop(){
-  if (digitalRead(F) == 0){
-    Serial.println("F");
-  }
-  else if(digitalRead(B) == 0){
-    Serial.println("B");
-  }
-  else if(digitalRead(L) == 0){
-    Serial.println("L");
-  }
-  else if(digitalRead(R) == 0){
-    Serial.println("R");
-  }
-  else{
-    Serial.println("S");
+  int sensorValue  = digitalRead(stopBtn);
+  if (sensorValue == 0||isStopBtnPressed == true){
+    //按下停止按钮后改变判断值，进入死循环，只有重启可以解除该状态
+    isStopBtnPressed = true;
+    return;
   }
 }
 
