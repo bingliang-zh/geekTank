b6299751553fc2f082791f1e3d09315a0ca20bcd
秉亮 <bl_indie@qq.com>
2015-08-24 00:37:27 +0800

    完善摇杆输出
    由ZY提供数学支持

--- arduino/controller/controller.ino
+++ arduino/controller/controller.ino
@@ -16,6 +16,7 @@ bool isStopBtnPressed = false;
 //基准量
 int benchmarkX = 512;
 int benchmarkY = 512;
+int allowError = 5;//电涌误差
 
 void benchmarkSetup(){
   int i;
@@ -28,6 +29,77 @@ void benchmarkSetup(){
   }
   benchmarkX = xSum / i;
   benchmarkY = ySum / i;
+}
+
+//手柄值归一量
+double xNol;
+double yNol;
+//履带归一值
+double lTransNol;
+double rTransNol;
+//履带传输值
+int lTrans;
+int rTrans;
+
+//根据模拟量输出值
+void driveTracks(int xValue, int yValue){
+  int xDValue = xValue - benchmarkX;
+  int yDValue = yValue - benchmarkY;
+  if(abs(xDValue) < allowError && abs(yDValue) < allowError){
+    //在基准上下误差内
+    return;
+  }
+  if(xDValue >= 0){
+    xNol = 1.0 * xDValue / (1023-benchmarkX);
+  }
+  else{
+    xNol = 1.0 * xDValue / benchmarkX;
+  }
+  xNol = -xNol;//因为手柄电路上X轴是反的
+  if(yDValue >= 0){
+    yNol = 1.0 * yDValue / (1023-benchmarkY);
+  }
+  else{
+    yNol = 1.0 * yDValue / benchmarkY;
+  }
+  if(xNol >= 0 && yNol >= 0){
+    lTransNol = max(xNol, yNol);
+    rTransNol = yNol - xNol;
+  }
+  else if(xNol < 0 && yNol >= 0){
+    lTransNol = xNol + yNol;
+    rTransNol = max(-xNol, yNol);
+  }
+  //下部代码由ZY同学提供数学支持（我太笨了哦天）
+  else if(xNol < 0 && yNol < 0){
+//    lTransNol = -max(-xNol, -yNol);
+//    rTransNol = yNol - xNol;
+    lTransNol = min(xNol, yNol) - max(xNol, yNol);
+    if(xNol > yNol){
+      rTransNol = yNol;
+    }
+    else{
+      rTransNol = 2*yNol - xNol;
+    }
+  }
+  else if(xNol >= 0 && yNol < 0){
+//    lTransNol = xNol + yNol;
+//    rTransNol = -max(xNol, -yNol);
+    rTransNol = min(-xNol, yNol) - max(-xNol, yNol);
+    if(-xNol > yNol){
+      lTransNol = yNol;
+    }
+    else{
+      lTransNol = 2*yNol + xNol;
+    }
+  }
+  Serial.print(xNol);
+  Serial.print("  ");
+  Serial.print(yNol);
+  Serial.print("  ");
+  Serial.print(lTransNol);
+  Serial.print("  ");
+  Serial.println(rTransNol);
 }
 
 void setup(){
@@ -83,7 +155,4 @@ void loop(){
 //  Serial.println(yValue-benchmarkY);  
 }
 
-void driveTracks(int xValue, int yValue){
-  
-}
 
